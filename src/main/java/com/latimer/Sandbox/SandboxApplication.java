8b5ac6c46994a76b520169b77a65103a64f18d6c
Ethan Latimer <ethan_t_latimer@homedepot.com>
2021-10-08 16:00:46 -0400

    modified file

--- src/main/java/com/latimer/Sandbox/SandboxApplication.java
+++ src/main/java/com/latimer/Sandbox/SandboxApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SandboxApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
-
 }
